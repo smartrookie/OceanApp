ac8c35b885fc65bdb82c96dd15d5aea633e2118d
rookie <1223352800@qq.com>
2016-11-18 14:20:14 +0800

    Initial Commit

---
+++ OceanApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OceanApp
+//
+//  Created by rookie on 2016/11/18.
+//  Copyright © 2016年 rookie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
